efd98d2b1e85838b8fe4396af4a6e8726ac725df
Joshen Lim <joshenlim@Joshens-MacBook-Pro.local>
2019-09-29 02:13:27 +0800

    Attendance sheet course filter

--- src/scripts/AttendanceTrack.js
+++ src/scripts/AttendanceTrack.js
@@ -5,19 +5,55 @@ export default {
   data() {
     return {
       courseCodeSearch: "",
+      courseFilterResults: [],
+      selectedCourse: {},
       selectedCourseGroup: "",
-      studentList: [],
-      attendanceStatus: ["Null", "Present", "Absent", "MC", "LOA"]
+      studentList: null,
+      attendanceStatus: ["Null", "Present", "Absent", "MC", "LOA"],
+      showSearch: true,
+      showGroupList: false,
     }
   },
   methods: {
-
+    toggleSearch: function() {
+      this.showSearch = true
+      this.searchCourseCode()
+    },
+    toggleGroupList: function() {
+      this.showGroupList = !this.showGroupList
+    },
+    searchCourseCode: function() {
+      this.$http.get(this.$apiUrl + '/courses?search=' + this.courseCodeSearch)
+        .then((result) => {
+          this.courseFilterResults = result.data
+        })
+    },
+    selectCourse: function(course) {
+      this.selectedCourse = { ...course }
+      this.courseCodeSearch = course.code
+      this.showSearch = false
+      
+      if (course.groups.length > 0) {
+        this.selectedCourseGroup = course.groups[0]
+        this.updateLabGroupTrack()
+      } else {
+        this.selectedCourseGroup = "No Lab Groups found"
+      }
+    },
+    selectGroup: function(group) {
+      this.selectedCourseGroup = group
+      this.showGroupList = false
+      this.updateLabGroupTrack()
+    },
+    updateLabGroupTrack: function() {
+      this.$http.get(this.$apiUrl + `/groups?name=${this.selectedCourseGroup}`)
+        .then((result) => {
+          this.studentList = result.data.students
+          console.log(this.studentList)
+        })
+    }
   },
   mounted() {
     this.$store.commit('updateAuthentication', true)
-    this.$http.get(this.$apiUrl + '/groups?name=TS1')
-      .then((result) => {
-        this.studentList = result.data.students
-      })
   }
 }
--- src/views/AttendanceTrack.vue
+++ src/views/AttendanceTrack.vue
@@ -6,11 +6,21 @@
       <div class="field-input">
         <p>COURSE CODE</p>
         <input v-model="courseCodeSearch" @input="searchCourseCode()" @click="toggleSearch()" type="text" />
+        <ul class="search-list" v-bind:class="{ showSearchList: (courseCodeSearch.length > 0 && showSearch) }">
+          <li v-for="course in courseFilterResults" v-bind:key="course.code" v-on:click="selectCourse(course)">
+            {{ course.code }} - {{ course.name }}
+          </li>
+        </ul>
       </div>
 
       <div class="field-input">
         <p>LAB GROUP</p>
         <input v-model="selectedCourseGroup" @click="toggleGroupList()" class="dropdown" type="text" readonly />
+        <ul class="search-list" v-bind:class="{ showSearchList: showGroupList }">
+          <li v-for="group in selectedCourse.groups" v-bind:key="group" v-on:click="selectGroup(group)">
+            {{ group }}
+          </li>
+        </ul>
       </div>
     </div>
 
@@ -24,7 +34,12 @@
       <div class="column lab-att">Lab 4</div>
       <div class="column lab-att">Lab 5</div>
     </div>
-    <ul class="attendance-table">
+
+    <div class="empty-table" v-if="!studentList">
+      <p>Select a Course Code to begin viewing attendance records</p>
+    </div>
+
+    <ul class="attendance-table" v-else-if="studentList.length > 0">
       <li class="row" v-for="(student, index) in studentList" v-bind:key="student.matric">
         <div class="column id">{{ index + 1 }}</div>
         <div class="column matric">{{ student.matric }}</div>
@@ -34,6 +49,10 @@
         </div>
       </li>
     </ul>
+
+    <div class="empty-table" v-else>
+      <p>Lab Group has no students assigned to it</p>
+    </div>
 
     <router-link class="icon-link" to="/lab-select">
       Back
